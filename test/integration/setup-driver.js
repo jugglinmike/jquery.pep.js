ebf97e660b1d9c0379e1b54e5288ecf371727731
Mike Pennisi <mike@mikepennisi.com>
2014-08-12 07:46:44 -0400

    Use TravisCI's tunnel when available

--- test/integration/setup-driver.js
+++ test/integration/setup-driver.js
@@ -18,14 +18,26 @@ function setupChromeDriver() {
   });
 }
 
-function setupSauceDriver(name, key, tunnelId) {
-  var Tunnel = require("sauce-tunnel");
+/**
+ * Set up a secure tunnel to Sauce Labs. This is done automatically in the CI
+ * environment [1] so that multiple test runs can share the same tunnel
+ * connection.
+ *
+ * [1] http://docs.travis-ci.com/user/sauce-connect/
+ *
+ * @param {String} name The Sauce Labs account name to use to create the
+ *                      tunnel.
+ * @param {String} key The Sauce Labs account key to use to create the tunnel.
+ *
+ * @returns {String} Identifier for the tunnel connection.
+ */
+function setupSetupSauceTunnel(name, key) {
+  var tunnelId = 'tunnel-' + (new Date()).getTime();
   var tunnel;
 
   before(function(done) {
     this.timeout(60 * 1000);
     tunnel = new Tunnel(name, key, tunnelId, true);
-
 
     tunnel.start(function(status) {
       if (status !== true) {
@@ -35,6 +47,29 @@ function setupSauceDriver(name, key, tunnelId) {
       done();
     });
   });
+
+  after(function(done) {
+    this.timeout(61 * 1000);
+    tunnel.stop(done);
+  });
+
+  return tunnelId;
+}
+
+/**
+ * @param {String} name The Sauce Labs account name to use to run the tests.
+ * @param {String} key The Sauce Labs account key to use to run the tests.
+ * @param {String} [tunnelId] The identifier of the currently-active tunnel to
+ *                            Sauce Labs, if any. When unspecified, a tunnel
+ *                            will be created prior to running the tests.
+ */
+function setupSauceDriver(name, key, tunnelId) {
+  var Tunnel = require("sauce-tunnel");
+  var tunnel;
+
+  if (!tunnelId) {
+    tunnelId = setupSauceTunnel(name, key);
+  }
 
   beforeEach(function() {
     this.driver = new webdriver.Builder()
@@ -52,8 +87,6 @@ function setupSauceDriver(name, key, tunnelId) {
   after(function(done) {
     // TODO: Annotate job with test status via Sauce Lab's REST API:
     // https://docs.saucelabs.com/reference/test-configuration/#job-annotation-with-the-rest-api
-    this.timeout(60 * 1000);
-    tunnel.stop(done);
   });
 }
 
@@ -64,7 +97,7 @@ module.exports = function() {
   var tunnelId = env.SAUCE_TUNNEL_ID;
   var driver, server;
 
-  if (sauceName && sauceKey && tunnelId) {
+  if (sauceName && sauceKey) {
     setupSauceDriver(sauceName, sauceKey, tunnelId);
   } else {
     setupChromeDriver();

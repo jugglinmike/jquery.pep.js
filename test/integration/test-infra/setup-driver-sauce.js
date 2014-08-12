e0ad9ac97da13c6f0c8109d1d579d9ef1f0bc917
Mike Pennisi <mike@mikepennisi.com>
2014-08-12 10:25:54 -0400

    Annotate Sauce Labs job with test status
    This necessitates sharing a driver instances between tests.

--- test/integration/test-infra/setup-driver-sauce.js
+++ test/integration/test-infra/setup-driver-sauce.js
@@ -1,6 +1,9 @@
 'use strict';
+var request = require('https').request;
+
 var webdriver = require('selenium-webdriver');
 var Tunnel = require("sauce-tunnel");
+var Promise = require('bluebird');
 
 /**
  * Set up a secure tunnel to Sauce Labs. This is done automatically in the CI
@@ -33,13 +36,61 @@ function setupSauceTunnel(name, key) {
   });
 
   after(function(done) {
-    // TODO: Annotate job with test status via Sauce Lab's REST API:
-    // https://docs.saucelabs.com/reference/test-configuration/#job-annotation-with-the-rest-api
     this.timeout(60 * 1000);
     tunnel.stop(done);
   });
 
   return tunnelId;
+}
+
+/**
+ * Annotate job with test status via Sauce Lab's REST API:
+ * https://docs.saucelabs.com/reference/test-configuration/#job-annotation-with-the-rest-api
+ *
+ * @param {String} name The Sauce Labs account name to use to create the
+ *                      tunnel.
+ * @param {String} key The Sauce Labs account key to use to create the tunnel.
+ * @param {String} jobId The Sauce Labs job identifier.
+ * @param {Boolean} allPassed The test state to report for the given job.
+ *
+ * @returns {Promise} An eventual value that will resolve when the request has
+ *                    completed.
+ */
+function annotateJob(name, key, jobId, allPassed) {
+  var body = JSON.stringify({ passed: allPassed });
+  var options = {
+    hostname: 'saucelabs.com',
+    path: '/rest/v1/' + name + '/jobs/' + jobId,
+    method: 'PUT',
+    auth: name + ':' + key,
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': body.length
+    }
+  };
+
+  return new Promise(function(resolve, reject) {
+    var req = request(options, function(res) {
+      var data = '';
+
+      res.on('data', function(chunk) {
+        data += chunk;
+      });
+
+      res.on('end', function() {
+        if (res.statusCode !== 200) {
+          reject(data);
+        } else {
+          resolve();
+        }
+      });
+    });
+
+    req.on('error', reject);
+
+    req.write(body);
+    req.end();
+  });
 }
 
 /**
@@ -50,7 +101,8 @@ function setupSauceTunnel(name, key) {
  *                            will be created prior to running the tests.
  */
 module.exports = function(name, key, tunnelId, buildId) {
-  var capabilities = {};
+  var allPassed = true;
+  var capabilities;
 
   if (!tunnelId) {
     tunnelId = setupSauceTunnel(name, key);
@@ -68,15 +120,25 @@ module.exports = function(name, key, tunnelId, buildId) {
     capabilities.build = buildId;
   }
 
-  beforeEach(function() {
+  before(function() {
     this.driver = new webdriver.Builder()
       .usingServer('http://ondemand.saucelabs.com:80/wd/hub')
       .withCapabilities(capabilities)
       .build();
   });
 
-  after(function(done) {
-    // TODO: Annotate job with test status via Sauce Lab's REST API:
-    // https://docs.saucelabs.com/reference/test-configuration/#job-annotation-with-the-rest-api
+  afterEach(function() {
+    if (this.currentTest.state !== 'passed') {
+      allPassed = false;
+    }
+  });
+
+  after(function() {
+    this.timeout(30 * 1000);
+
+    return this.driver.getSession()
+      .then(function(session) {
+        return annotateJob(name, key, session.getId(), allPassed);
+      });
   });
 };

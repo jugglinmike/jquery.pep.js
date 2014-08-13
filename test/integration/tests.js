1584ef313d5e8dad134bdc987e4a7f15dbff5a81
Mike Pennisi <mike@mikepennisi.com>
2014-08-12 21:02:10 -0400

    Add more debugging statements

--- test/integration/tests.js
+++ test/integration/tests.js
@@ -35,10 +35,15 @@ describe('jQuery.pep', function() {
     it('enables dragging and dropping', function() {
       var pep = this.peps[0];
       var origLocation;
+      var start = Date.now();
+      var log = function(msg) {
+        console.log(msg, Date.now() - start);
+      };
 
       return pep.getLocation()
         .then(function(location) {
           origLocation = location;
+          log('starting to drag');
           return driver.actions()
             .mouseDown(pep)
             .mouseMove({ x: 10, y: 20 })
@@ -47,12 +52,16 @@ describe('jQuery.pep', function() {
           // Wait before dropping to prevent intertial movement. This magic
           // constant is unfortunate but unavoidable because there is no
           // external indication of the "momentum" of a given element.
+          log('waiting');
           return driver.sleep(500);
         }).then(function() {
+          log('releasing');
           return driver.actions().mouseUp().perform();
         }).then(function() {
+          log('requesting location');
           return pep.getLocation();
         }).then(function(newLocation) {
+          log('asserting');
           assert.equal(origLocation.x + 10, newLocation.x);
           assert.equal(origLocation.y + 20, newLocation.y);
         });
